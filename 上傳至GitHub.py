faae4844816e597790066ea03bba9fb459ffe6e3
bang <maxchao1217@gmail.com>
2018-08-21 16:04:09 +0800

    oo

---
+++ 上傳至GitHub.py
@@ -0,0 +1,20 @@
+echo "# python_code" >> README.md
+
+git config --global user.name "bang"
+git config --global user.email "maxchao1217@gmail.com"
+
+
+
+
+git init
+git add README.md
+git commit -m "新增README.md"
+git remote add origin https://github.com/lagigo/python_code.git
+git push -u origin master
+
+
+
+
+git add README.md
+git commit -m "新增README.md"
+git push -u origin master
