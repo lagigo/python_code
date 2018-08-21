35b79cbe596fca6558a835c95f649f779b9a7e54
bang <maxchao1217@gmail.com>
2018-08-21 15:59:10 +0800

    compare

---
+++ compare.py
@@ -0,0 +1,13 @@
+num1 = float(input("輸入數1:"))
+num2 = float(input("輸入數2:"))
+
+
+if num1>num2 :
+    str(num1)
+    str(num2)
+    print (num1,"大於",num2)
+elif num1<num2 :
+    print (num1,"小於",num2)
+else :
+    print (num1,"等於",num2)
+
