98bd32ff57c8d9ff33134189dd2608f7908d08a8
bang <maxchao1217@gmail.com>
2018-08-21 14:54:00 +0800

    '相撲'

---
+++ 相撲.py
@@ -0,0 +1,12 @@
+age = int (input('請輸入年齡:'))
+weight = int (input('請輸入體重:'))
+x = 'yes'
+y = input('男?yes/no :')
+
+
+if age>=20 and weight>=100 and y==x :
+    print('你可以當相撲選手')
+
+else:
+    print('你不能成為相撲選手')
+
