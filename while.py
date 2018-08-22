db29d571a797033769c0ae853057c7ff315c2716
bang <maxchao1217@gmail.com>
2018-08-22 17:02:02 +0800

    'fbi'

---
+++ while.py
@@ -0,0 +1,9 @@
+import webbrowser
+age = int(input ('type your age:'))
+while age >=18:
+    print ("i'm 13 years old girl. Want to bangbang? yes/no  1/2")
+    if int(input())== 1 :
+        webbrowser.open('https://www.fbi.gov/')
+    else:
+        break
+print ('滾')
